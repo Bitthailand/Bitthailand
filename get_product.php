0e30c2f4d8b3e534e00f8c3d7d9447106f795917
akitcom2004 <akitcom2004@gmail.com>
2021-08-10 16:20:37 +0700

    แนท

---
+++ get_product.php
@@ -0,0 +1,10 @@
+<?php
+include './include/connect.php';
+$sql = "SELECT * FROM product  WHERE ptype_id={$_GET['ptype_id']} AND  width={$_GET['width']}";
+$query = mysqli_query($conn, $sql);
+$json = array();
+while($result = mysqli_fetch_assoc($query)) {    
+array_push($json, $result);
+}
+echo json_encode($json);
+?>
