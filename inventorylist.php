d653dbffe78fe8255774295c6277eb961b41b836
akitcom2004 <akitcom2004@gmail.com>
2021-09-06 16:25:10 +0700

    com

--- inventorylist.php
+++ inventorylist.php
@@ -197,12 +197,12 @@ if ($rowS == '') {
                                         $next_page = $page_no + 1;
                                         $adjacents = "2";
 
-                                        $result_count = mysqli_query($conn, "SELECT COUNT(*) As total_records FROM product  where  status='0' AND  ptype_id<>'TF0' $columx $keywordx  ");
+                                        $result_count = mysqli_query($conn, "SELECT COUNT(*) As total_records FROM product  where  status='0' AND  ptype_id<>'TF0' $columx $keywordx  order by product_id asc ");
                                         $total_records = mysqli_fetch_array($result_count);
                                         $total_records = $total_records['total_records'];
                                         $total_no_of_pages = ceil($total_records / $total_records_per_page);
                                         $second_last = $total_no_of_pages - 1; // total page minus 1
-                                        $result = mysqli_query($conn, "SELECT * FROM product where status='0' AND ptype_id <> 'TF0' $columx $keywordx LIMIT $offset, $total_records_per_page");
+                                        $result = mysqli_query($conn, "SELECT * FROM product where  ptype_id <> 'TF0' AND  status='0' $columx $keywordx order by product_id asc  LIMIT $offset, $total_records_per_page");
                                         while ($row = mysqli_fetch_array($result)) { ?>
                                             <tr>
                                                 <td><?php echo $row["product_id"]; ?></td>
