94b8fed383ed2c5bd0dc744f2b317054a32abcb4
akitcom2004 <akitcom2004@gmail.com>
2021-09-10 14:43:51 +0700

    comx

--- saleorder.php
+++ saleorder.php
@@ -68,7 +68,7 @@ $row5 = $rs5->fetch_assoc();
         <div class="col-12">
         <div class="row">
             <div class="col-2_logo">
-                <img class="logo" src="../../dist-assets/images/logo1.png" alt="">
+                <img class="logox" src="../../dist-assets/images/logo1.png" alt="">
             </div>
             <div class="col-5">
                 <h4 class="font-weight-bold">บริษัท วันเอ็ม จำกัด</h4>
