a75de2d1e6ed956cded43ada0a697ebe1b6b6fc4
akitcom2004 <akitcom2004@gmail.com>
2021-09-11 13:51:08 +0700

    x

--- quotation.php
+++ quotation.php
@@ -33,7 +33,7 @@ $strNewDate = date("Y-m-d", strtotime("+$row[date_confirm] day", strtotime($strS
     <title>Quotation | ใบเสนอราคา</title>
     <!-- <link rel="stylesheet" href="style.css" /> -->
     <!-- <link href="../../dist-assets/css/themes/lite-purple.min.css" rel="stylesheet" /> -->
-    <link href="../../dist-assets/css/themes/styleforprint.css?v=9" rel="stylesheet" />
+    <link href="../../dist-assets/css/themes/styleforprint.css?v=10" rel="stylesheet" />
 
 
     <style>
