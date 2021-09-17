d86c38dba7edfc52cc70d910f976ae0ed01aa609
akitcom2004 <akitcom2004@gmail.com>
2021-09-17 14:56:06 +0700

    v

--- include/connect.php
+++ include/connect.php
@@ -11,7 +11,7 @@ mysqli_query($conn,"SET SESSION collation_connection ='utf8_unicode_ci'");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+// ssmflv[]
 
 // $host = "localhost"; /* Host name */
 // $user = "root"; /* User */
