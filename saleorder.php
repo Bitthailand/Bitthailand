130f414472718e30f3a3a8ae05a60431873fb1ee
akitcom2004 <akitcom2004@gmail.com>
2021-09-21 09:02:49 +0700

    x

--- saleorder.php
+++ saleorder.php
@@ -280,7 +280,7 @@ $row5 = $rs5->fetch_assoc();
                     </div>
                     <div class="col-4xx_cus">
                         <p style="font-size: 18px;"><?= $row3['customer_name'] ?></p>
-                        <p style="font-size: 18px;"><?php if ($rowx3_TF['product_name'] == '') {
+                        <p style="font-size: 18px;"><?php echo $row5['cus_back']; if ($rowx3_TF['product_name'] == '') {
                                                        
                                                         echo $row3['bill_address'] . " $t" . $row6['name_th'] . "  $a" . $row7['name_th'] . " จ." . $row8['name_th'];
                                                     } else {
