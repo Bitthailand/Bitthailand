9e57635de149152a0dfabcba4eb4a57a2395eea4
akitcom2004 <akitcom2004@gmail.com>
2021-09-24 12:22:16 +0700

    ป

--- ordersuccesslist.php
+++ ordersuccesslist.php
@@ -16,7 +16,7 @@ unset($_SESSION['order_id']);
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Datatables | Gull Admin Template</title>
+    <title>รายการสำเร็จ</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,400i,600,700,800,900" rel="stylesheet" />
     <link href="../../dist-assets/css/themes/lite-purple.min.css" rel="stylesheet" />
     <link href="../../dist-assets/css/plugins/perfect-scrollbar.min.css" rel="stylesheet" />
