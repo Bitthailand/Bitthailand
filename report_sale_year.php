60f5ed87da4d95e9a7a91fc181208a2114a7a4df
akitcom2004 <akitcom2004@gmail.com>
2021-09-24 14:56:14 +0700

    x

--- report_sale_year.php
+++ report_sale_year.php
@@ -21,7 +21,7 @@ if (isset($_SESSION["username"])) {
 <?php
 include './include/connect.php';
 include './include/config.php';
-include './get_dashbord.php';
+include './get_dashbord_production_year.php';
 $datex = date('Y-m-d');
 $d = explode("-", $datex);
 $sql_pday = "SELECT count(production_detail.product_id) AS today FROM production_order INNER JOIN production_detail ON production_order.po_id=production_detail.po_id AND 
@@ -29,15 +29,6 @@ production_order.po_enddate LIKE  '$datex%'  ";
 $rs_pday = $conn->query($sql_pday);
 $row_pday = $rs_pday->fetch_assoc();
 
-$sql_pdaycf = "SELECT count(production_detail.product_id) AS today FROM production_order INNER JOIN production_detail ON production_order.po_id=production_detail.po_id AND 
-production_order.po_enddate LIKE  '$datex%' AND production_detail.status_stock='0'  ";
-$rs_pdaycf = $conn->query($sql_pdaycf);
-$row_pdaycf = $rs_pdaycf->fetch_assoc();
-
-$sql_pdaycf1 = "SELECT count(production_detail.product_id) AS today FROM production_order INNER JOIN production_detail ON production_order.po_id=production_detail.po_id AND 
-production_order.po_enddate LIKE  '$datex%' AND production_detail.status_stock='1'  ";
-$rs_pdaycf1 = $conn->query($sql_pdaycf1);
-$row_pdaycf1 = $rs_pdaycf1->fetch_assoc();
 
 ?>
 
@@ -72,7 +63,7 @@ $row_pdaycf1 = $rs_pdaycf1->fetch_assoc();
             <div class="tab-content">
                 <div class="main-content">
 
-                    <div class="breadcrumb">
+                <div class="breadcrumb">
                         <h1 class="mr-2">ข้อมูลยอดขาย
                         </h1>
                         <ul>
@@ -80,12 +71,14 @@ $row_pdaycf1 = $rs_pdaycf1->fetch_assoc();
                        
                         </ul>
                     </div>
-                    
+
+
+
                     <div class="row">
                         <div class="col-lg-12 col-md-12">
                             <div class="card mb-4">
                                 <div class="card-body">
-                                    <div class="card-title">ยอดขายประจำปี</div>
+                                    <div class="card-title">ยอดผลิตประจำปี</div>
                                     <div id="echartBar" style="height: 300px;"></div>
                                 </div>
                             </div>
@@ -101,99 +94,64 @@ $row_pdaycf1 = $rs_pdaycf1->fetch_assoc();
 
                                             <div class="ul-widget__head">
                                                 <div class="ul-widget__head-label">
-                                                    <h3 class="ul-widget__head-title">รายการขายประจำเดือน</h3>
+                                                    <h3 class="ul-widget__head-title">รายการสั่งผลิตสินค้าแบบรายปี</h3>
                                                 </div>
                                                 
                                             </div>
-                                            <div class="ul-widget__body">
-                                                <div class="tab-content">
-                                                    <div class="tab-pane active show" id="__d-widget4-tab1-content">
-                                                        <div class="ul-widget1">
 
-                                                            <div class="table-responsive">
-                                                                <table class="table text-center" id="user_table">
-                                                                    <thead>
-                                                                        <tr>
-                                                                            <th scope="col">#</th>
-                                                                            <th scope="col" class="text-right">วันที่</th>
-                                                                            <th scope="col" class="text-right">ลูกค้า</th>
-                                                                            <th scope="col" class="text-right">รายการ</th>
-                                                                            <th scope="col" class="text-right">ยอดมัดจำ</th>
-                                                                            <th scope="col" class="text-right">ยอดขาย</th>
-                                                                            <th scope="col" class="text-right">ยอดคืน</th>
-                                                                            <th scope="col" class="text-right">ข้อมูล</th>
-                                                                        </tr>
-                                                                    </thead>
-                                                                    <tbody>
-                                                                        <?php $sql4 = "SELECT  DATE_FORMAT(dev_date, '%Y-%m-%d') AS DATE   FROM delivery WHERE  status_chk='1' AND status_payment='1'  AND dev_date  BETWEEN NOW() - INTERVAL 30 DAY AND NOW() GROUP BY DATE ORDER BY DATE DESC";
-                                                                        $result4 = mysqli_query($conn, $sql4);
-                                                                        if (mysqli_num_rows($result4) > 0) {
-                                                                            while ($row4 = mysqli_fetch_assoc($result4)) {
-                                                                                $d = explode("-", $row4['DATE']);
-                                                                                $sql_cus_day = "SELECT COUNT(DISTINCT cus_id) month FROM delivery  WHERE DAY(dev_date)= '$d[2]' AND MONTH(dev_date) = '$d[1]' AND YEAR(dev_date) = '$d[0]' AND status_chk='1' AND status_payment='1'  ";
-                                                                                $rs_cus_day = $conn->query($sql_cus_day);
-                                                                                $row_cus_day = $rs_cus_day->fetch_assoc();
-                                                                                $sql_dev = "SELECT COUNT(DISTINCT dev_id) dev FROM delivery  WHERE DAY(dev_date)= '$d[2]' AND MONTH(dev_date) = '$d[1]' AND YEAR(dev_date) = '$d[0]' AND status_chk='1' AND status_payment='1'  ";
-                                                                                $rs_dev = $conn->query($sql_dev);
-                                                                                $row_dev = $rs_dev->fetch_assoc();
-                                                                                $sql_sum = "SELECT SUM(deliver_detail.total_price)AS total  FROM deliver_detail
-                                                                                INNER JOIN  delivery ON delivery.dev_id=deliver_detail.dev_id  AND
-                                                                                DAY(delivery.dev_date)= '$d[2]' AND MONTH(delivery.dev_date) = '$d[1]' AND YEAR(delivery.dev_date) = '$d[0]' AND delivery.status_payment='1' ";
-                                                                                $rs_sum = $conn->query($sql_sum);
-                                                                                $row_sum = $rs_sum->fetch_assoc();
 
-                                                                                $sql_refun = "SELECT SUM(total_price)AS total  FROM sr_detail WHERE
-                                                                                DAY(date_create)= '$d[2]' AND MONTH(date_create) = '$d[1]' AND YEAR(date_create) = '$d[0]' ";
-                                                                                $rs_refun = $conn->query($sql_refun);
-                                                                                $row_refun = $rs_refun->fetch_assoc();
-
-                                                                                $sql_ai = "SELECT SUM(price)AS total  FROM ai_number  WHERE
-                                                                                DAY(date_create)= '$d[2]' AND MONTH(date_create) = '$d[1]' AND YEAR(date_create) = '$d[0]' ";
-                                                                                $rs_ai = $conn->query($sql_ai);
-                                                                                $row_ai = $rs_ai->fetch_assoc();
-                                                                        ?>
-                                                                                <tr>
-                                                                                    <th scope="row"><?= ++$idx; ?></th>
-                                                                                    <td class="text-left">
-                                                                                        <?php echo "$row4[DATE]"; ?>
-                                                                                    </td>
-                                                                                    <td class="text-right"><?php echo number_format($row_cus_day['month'], '0', '.', ',') ?></td>
-                                                                                    <td class="text-right"><?php echo number_format($row_dev['dev'], '0', '.', ',') ?></td>
-                                                                                    <td class="text-right"><?php echo number_format($row_ai['total'], '2', '.', ',') ?></td>
-                                                                                    <td class="text-right"><?php echo number_format($row_sum['total'], '2', '.', ',') ?></td>
-                                                                                    <td class="text-right"><?php echo number_format($row_refun['total'], '2', '.', ',') ?></td>
-                                                                                    <td class="text-right"><a class="btn btn-outline-success btn-sm line-height-1" data-toggle="tooltip" title="ดูข้อมูลรายวัน" href="/report_sale_date.php?MyDate=<?= $row4['DATE'] ?>">
-                                                                                            <i class="i-Check font-weight-bold"></i> </a></td>
-
-                                                                                </tr>
-                                                                        <?php }
-                                                                        } ?>
-
-                                                                    </tbody>
-                                                                </table>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                      
-
-                                                </div>
+                                            <div class="table-responsive">
+                                                <table class="table text-center" id="user_table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">#</th>
+                                                            <th scope="col" class="text-left">ชื่อสินค้า</th>
+                                                            <th scope="col" class="text-left">ผลิต</th>
+                                                            <th scope="col" class="text-left">สำเร็จ</th>
+                                                            <th scope="col" class="text-left">ชำรุด</th>
+                                                            <th scope="col" class="text-left">มูลค่า</th>
+                                                            <th scope="col" class="text-left">ข้อมูล</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php $sql4 = "SELECT DATE_FORMAT(po_enddate,'%Y') As MyYear,SUM(production_detail.qty) AS qty ,production_detail.product_id AS product_id,SUM(production_detail.a_type) AS a_type ,SUM(production_detail.b_type) AS b_type ,SUM(product.unit_price)AS unit_price,SUM(qty*unit_price) AS sumall ,SUM(a_type*unit_price) AS sum_atype ,SUM(b_type*unit_price) AS sum_btype   FROM production_detail  INNER JOIN  product  ON product.product_id=production_detail.product_id  
+                                                                                    INNER JOIN  production_order ON  production_order.po_id=production_detail.po_id  AND production_detail.status_stock='1'  GROUP BY MyYear  ORDER BY qty DESC  ";
+                                                        $result4 = mysqli_query($conn, $sql4);
+                                                        if (mysqli_num_rows($result4) > 0) {
+                                                            while ($row4 = mysqli_fetch_assoc($result4)) {
+                                                        ?> <tr>
+                                                                    <th scope="row"><?= ++$idx; ?></th>
+                                                                    <td class="text-left">
+                                                                        <?php $sql_pro = "SELECT * FROM product   WHERE product_id= '$row4[product_id]'";
+                                                                        $rs_pro = $conn->query($sql_pro);
+                                                                        $row_pro = $rs_pro->fetch_assoc();
+                                                                        $year =$row4['MyYear'] + 543;  ?>
+                                                                        <?= $year ?></td>
+                                                                    <td class="text-left"><?php echo number_format($row4['qty'], '0', '.', ',') ?></td>
+                                                                    <td class="text-left"><?php echo number_format($row4['a_type'], '0', '.', ',') ?></td>
+                                                                    <td class="text-left"><?php echo number_format($row4['b_type'], '0', '.', ',') ?></td>
+                                                                    <td class="text-left"><?php echo number_format($row4['sumall'], '2', '.', ',') ?></td>
+                                                                    <td class="text-left"><a class="btn btn-outline-success btn-sm line-height-1" data-toggle="tooltip" title="ดูข้อมูลรายเดือน" href="/report_production_month.php?MyYear=<?= $row4['MyYear'] ?>">
+                                                        <i class="i-Check font-weight-bold"></i> </a></td>
+                                                                </tr>
+                                                        <?php }
+                                                        } ?>
+                                                    </tbody>
+                                                </table>
                                             </div>
-
                                         </div>
+
+
                                     </div>
                                 </div>
-
                             </div>
 
                         </div>
 
-
-
-                      
-
-                    </div><!-- end of main-content -->
+                    </div>
                 </div>
+
+
 
                 <!-- Header -->
                 <?php include './include/footer.php'; ?>
