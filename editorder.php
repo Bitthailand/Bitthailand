340e1cc0a09945fa09aa909b53d5852f271b1a8d
akitcom2004 <akitcom2004@gmail.com>
2021-09-24 11:12:47 +0700

    c

--- editorder.php
+++ editorder.php
@@ -152,8 +152,8 @@ if ($action == 'add_product') {
         $row5 = $rs5->fetch_assoc();
         // =======================
         $send_total = $send_price * $send_qty;
-        $sql = "INSERT INTO order_details (order_id,ptype_id,product_id,qty,unit_price,total_price,status_button,emp_id,status_chk_stock,qty_out,updates,qty_out)
-        VALUES ('$Forder_id','$Fproduct_type','$row5[product_id]','$send_qty','$send_price','$send_total','1','$emp_id','TF','$send_price','1','$send_qty')";
+        $sql = "INSERT INTO order_details (order_id,ptype_id,product_id,qty,unit_price,total_price,status_button,emp_id,status_chk_stock,qty_out,updates)
+        VALUES ('$Forder_id','$Fproduct_type','$row5[product_id]','$send_qty','$send_price','$send_total','1','$emp_id','TF','$send_price','1')";
         if ($conn->query($sql) === TRUE) { ?>
             <script>
                 $(document).ready(function() {
