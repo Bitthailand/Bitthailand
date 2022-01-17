92cb3d7149d973886bca4e834a64330fa70c979c
akitcom2004 <akitcom2004@gmail.com>
2022-01-17 10:13:44 +0700

    ปปป

--- quotationlist.php
+++ quotationlist.php
@@ -16,6 +16,10 @@ if ($conn->query($sql) === TRUE) {
 $sql2 = "DELETE FROM order_details  WHERE status_button='0' AND emp_id='$emp_id'  ";
 if ($conn->query($sql2) === TRUE) {
 }
+$event_msg="อยู่หน้ารายการสั่งซื้อ";
+$sql_event = "INSERT INTO log (emp_id,event)
+VALUES ('$emp_id,'$event_msg')";
+if ($conn->query($sql_event ) === TRUE) {  }
 // $sql3 = "UPDATE orders_number  SET  status_use='2',status_cf='0'  where emp_id='$emp_id' AND status_button='0' ";
 // if ($conn->query($sql3) === TRUE) {}
 // $sql3 = "DELETE FROM orders_number  WHERE status_use='1' AND emp_id='$emp_id'  ";
