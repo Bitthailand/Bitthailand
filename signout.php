b8709b6d69290fca5d8109b5d9b38660e8b207c7
akitcom2004 <akitcom2004@gmail.com>
2022-02-24 12:55:19 +0700

    xxx

--- signout.php
+++ signout.php
@@ -4,6 +4,6 @@ unset($_SESSION['username']);
 unset($_SESSION['user']);
 
 
-session_destroy(); // ทำลาย session
+// session_destroy(); // ทำลาย session
 //session_destroy();
 echo "<script>window.location = 'signin.php'</script>";
